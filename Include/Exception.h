27699cba84e44c6f9e337dd00d5e0c129bedff4d
bnulwh@gmail.com <bnulwh@gmail.com@dddd1382-7c0c-11de-9529-154b375120ac>
2009-08-21 06:54:05 +0000

    TO VC6 DLL

---
+++ Include/Exception.h
@@ -0,0 +1,20 @@
+// Exception.h: interface for the Exception class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_EXCEPTION_H__50BF5321_C967_4688_9609_69634FE6CB0C__INCLUDED_)
+#define AFX_EXCEPTION_H__50BF5321_C967_4688_9609_69634FE6CB0C__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class Exception  
+{
+public:
+	Exception();
+	virtual ~Exception();
+
+};
+
+#endif // !defined(AFX_EXCEPTION_H__50BF5321_C967_4688_9609_69634FE6CB0C__INCLUDED_)
